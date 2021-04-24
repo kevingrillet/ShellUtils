63b4ac40681280850b9d7fc2335f28e36dc35a11
Kévin GRILLET <kevin@MacBook-Pro-de-Kevin.local>
2021-04-24 08:52:51 +0200

    Update utils_stdout_with_colors.sh
    - printDebug

--- utils_stdout_with_colors.sh
+++ utils_stdout_with_colors.sh
@@ -4,32 +4,37 @@
 [ -n "${UTILS_STDOUT_WITH_COLORS}" ] && return; UTILS_STDOUT_WITH_COLORS=0; #pragma once
 source utils_stdout_colors.sh
 
+# printDebug <MESSAGE>
+function printDebug() {
+    echo -e "${On_Blue}[DEBUG] $1${Color_Off} "
+}
+
 # printError <MESSAGE>
 function printError() {
-    echo -e "${Red}Error:${Color_Off} $1"
+    echo -e "${Red}[ERROR]${Color_Off} $1"
 }
 
 # printInfo <MESSAGE>
 function printInfo() {
-    echo -e "${Cyan}Info:${Color_Off} $1"
+    echo -e "${Cyan}[INFORMATION]]${Color_Off} $1"
 }
 
 # printSuccess <MESSAGE>
 function printSuccess() {
-    echo -e "${Green}Success:${Color_Off} $1"
+    echo -e "${Green}[SUCCESS]${Color_Off} $1"
 }
 
 # printTask <MESSAGE>
 function printTask() {
-    echo -e "${Blue}Task:${Color_Off} $1"
+    echo -e "${Blue}[TASK]${Color_Off} $1"
 }
 
 # printTip <MESSAGE>
 function printTip() {
-    echo -e "${Purple}Tip:${Color_Off} $1"
+    echo -e "${Purple}[TIP]${Color_Off} $1"
 }
 
 # printWarn <MESSAGE>
 function printWarn() {
-    echo -e "${Yellow}Warning:${Color_Off} $1"
+    echo -e "${Yellow}[WARNING]${Color_Off} $1"
 }
