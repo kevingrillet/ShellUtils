cd021bb8d12ee59a70f0edf76cd4d217ee46b86c
Kévin GRILLET <kevin@MacBook-Pro-de-Kevin.local>
2021-04-24 08:44:03 +0200

    Update utils.sh
    - tab 2 > 4 spaces
    - my_exec > execVerbose
    - execDebug

--- utils.sh
+++ utils.sh
@@ -3,28 +3,38 @@
 # https://www.shellcheck.net/
 [ -n "${UTILS}" ] && return; UTILS=0; #pragma once
 
-# my_exec <COMMAND>
-# hide the output if verbose is false
-function my_exec {
-  if [ $VERBOSE = true ] ;
-  then
-    "$@"
-  else
-    "$@" &>/dev/null
-  fi
+DEBUG=true
+VERBOSE=false
+
+# execDebug <COMMAND>
+# exec if DEBUG=true
+function execDebug() {
+    if [ $DEBUG = true ]; then
+        "$@"
+    fi
+}
+# source utils_stdout_with_colors.sh; execDebug printWarn "test"
+
+# execVerbose <COMMAND>
+# hide the output if VERBOSE=false
+function execVerbose() {
+    if [ $VERBOSE = true ]; then
+        "$@"
+    else
+        "$@" &>/dev/null
+    fi
 }
 
 # pause
-function pause {
-  read -s -r -n 1 -p "Press any key to continue..."
-  echo ""
+function pause() {
+    read -s -r -n 1 -p "Press any key to continue..."
+    echo ""
 }
 
 # printVerbose <MESSAGE>
 # print if verbose
-function printVerbose {
-  if [ $VERBOSE = true ] ;
-  then
-    echo -e "$1"
-  fi
+function printVerbose() {
+    if [ $VERBOSE = true ]; then
+        echo -e "$1"
+    fi
 }
