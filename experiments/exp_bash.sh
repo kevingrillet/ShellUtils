9fcef0065fcfe49fe61aad8d989a46ac849e381e
kevingrillet <kevin.grillet@gmail.com>
2021-05-01 16:07:54 +0200

    Update exp_bash.sh

--- experiments/exp_bash.sh
+++ experiments/exp_bash.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # ./experiments/exp_bash.sh
 
-bash --version
+bash --version                                  # Check bash version
+dos2unix ../*/*.sh                              # Reformat all .sh files: CR + LF => LF
