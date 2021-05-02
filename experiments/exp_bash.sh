0d3181717ed47a4039056efa3ee7feb5c04c4cdc
Kévin GRILLET <kevin@MacBook-Pro-de-Kevin.local>
2021-05-02 17:39:23 +0200

    Update exp_bash.sh

--- experiments/exp_bash.sh
+++ experiments/exp_bash.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-# ./experiments/exp_bash.sh
+# ##############################################################################
+# Script Name   : exp_bash.sh
+# Description   : Experiment around Bash
+# Author        : Kevin GRILLET
+# GitHub        : https://github.com/kevingrillet/ShellUtils
+# License       : GNU GPL-3
+# ##############################################################################
 
 bash --version                                  # Check bash version
-dos2unix ../*/*.sh                              # Reformat all .sh files: CR + LF => LF
+if command -v vi &> /dev/null; then             # Check if vi exists
+    vi -es +"bufdo wq" ../*/*.sh                # Adds EOL at EOF on file save if it's missing.
+fi
+if command -v dos2unix &> /dev/null; then       # Check if dos2unix exists
+    dos2unix ../*/*.sh                          # Reformat all .sh files: CR + LF => LF
+fi
 chmod -v +x ../*/*.sh                           # chmod +x all .sh files
